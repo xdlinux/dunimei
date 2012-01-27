72c5e3395db2f52f1fecb3e1ef5ddcccc3afbdc3
Sean Lee <iseansay@gmail.com>
2012-01-28 00:34:56 +0800

    add place, item(temp name) model, remove the Books m2m key in accounts.model

--- dnm/account/models.py
+++ dnm/account/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from books.models import Book
+from books.models import Place
+
 class UserProfile(models.Model):
     """hold the user's profile info"""
     user = models.OneToOneField(User)
     bio = models.CharField(blank=True, max_length=140)
     webpage = models.URLField(blank=True, verify_exists=False)
     phone = models.CharField(max_length=11)
-    books = models.ManyToManyField(Book)
+    place = models.ForeignKey(Place)
 
     def __unicode__(self):
         return u'profile'
--- dnm/books/models.py
+++ dnm/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     """the book model"""
@@ -6,7 +7,33 @@ class Book(models.Model):
     isbn = models.CharField(max_length=13)
     authors = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
-    
+
     def __unicode__(self):
         return self.title
 
+
+class Place(models.Model):
+    """
+    the infomation about a place(example: xidian, tsinghua)
+    slug: typically the midddle part of the school's domain name, for example
+          www.xidian.edu.cn => xidian, www.xahu.edu.cn => xahu, the slug is
+          use for url of the place.
+    custom_css:  inspire by reddit, the admin of a place can submit a custom
+                css to change the look of the page.
+    """
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=500)
+    custom_css = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Item(models.Model):
+    """"""
+    book = models.ForeignKey(Book)
+    current_record = models.ForeignKey(Record)
+
+    def __unicode__(self):
+        return u"Item"
